dc89fac1d78984247b632dcbbaa8d5d16fe098d7
Revaz Komakhidze <rezokomakhidze1@gmail.com>
2024-04-07 18:44:54 +0400

    added shadcn library

--- src/app/(root)/(home)/page.tsx
+++ src/app/(root)/(home)/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <h1 className="h1-bold">Home</h1>;
+  return <div>Home</div>;
 }
---
+++ src/context/ThemeProvider.tsx
@@ -0,0 +1,43 @@
+"use client";
+import React, { createContext, useContext, useEffect, useState } from "react";
+
+type ThemeContextType = {
+  mode: string;
+  handleThemeChange: (mode: string) => void;
+};
+
+const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
+
+export const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
+  const [mode, setMode] = useState("");
+
+  const handleThemeChange = () => {
+    if (mode === "dark") {
+      setMode("light");
+      document.documentElement.classList.add("light");
+    } else {
+      setMode("dark");
+      document.documentElement.classList.add("dark");
+    }
+  };
+
+  useEffect(() => {
+    handleThemeChange();
+  }, []);
+
+  return (
+    <ThemeContext.Provider value={{ mode, handleThemeChange }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export const useThemeContext = () => {
+  const context = useContext(ThemeContext);
+
+  if (!context) {
+    throw new Error("Theme context can only be accessed inside provider");
+  }
+
+  return context;
+};
