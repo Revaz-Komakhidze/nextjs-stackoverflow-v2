20738301d4d1efbbe8ae927e04e1f7e80ed83d86
Revaz Komakhidze <rezokomakhidze1@gmail.com>
2024-04-07 17:07:00 +0400

    added clerk auth and basic config for the root layout

--- src/app/(auth)/sign-in/page.tsx
+++
@@ -1,5 +0,0 @@
-const Signin = () => {
-  return <h1 className="h1-bold">Signin</h1>;
-};
-
-export default Signin;
--- src/app/(auth)/sign-up/page.tsx
+++
@@ -1,5 +0,0 @@
-const Signup = () => {
-  return <h1 className="h1-bold">Signup</h1>;
-};
-
-export default Signup;
