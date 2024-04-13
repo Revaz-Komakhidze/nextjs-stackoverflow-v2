4a6e54959226994e2db8de5e47ae82cee5e864f7
Revaz Komakhidze <rezokomakhidze1@gmail.com>
2024-04-13 11:08:00 +0400

    added filters for mobile and desktop screens

--- src/app/(root)/(home)/page.tsx
+++ src/app/(root)/(home)/page.tsx
@@ -1,5 +1,8 @@
+import HomeFilters from "@/components/home/HomeFilters";
+import Filter from "@/components/shared/Filter";
 import LocalSearchbar from "@/components/shared/search/LocalSearch";
 import { Button } from "@/components/ui/button";
+import { HomePageFilters } from "@/constants/filters";
 import Link from "next/link";
 
 // const questions = [
@@ -52,19 +55,23 @@ export default function Home() {
         </Link>
       </div>
 
-      <div className="mt-10 flex w-full flex-col gap-6">
-        <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
-          <LocalSearchbar
-            route="/"
-            iconPosition="left"
-            imgSrc="/assets/icons/search.svg"
-            placeholder="Search for questions"
-            otherClasses="flex-1"
-          />
-          Filter
-        </div>
-        HomeFilters
+      <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
+        <LocalSearchbar
+          route="/"
+          iconPosition="left"
+          imgSrc="/assets/icons/search.svg"
+          placeholder="Search for questions"
+          otherClasses="flex-1"
+        />
+
+        <Filter
+          filters={HomePageFilters}
+          otherClasses="min-h-[56px] sm:min-w-[170px]"
+          containerClasses="hidden max-md:flex"
+        />
       </div>
+
+      <HomeFilters />
     </>
   );
 }
