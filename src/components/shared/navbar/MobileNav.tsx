1aef3d301afe8b413d1d59d6f5d65556d658d32f
Revaz Komakhidze <rezokomakhidze1@gmail.com>
2024-04-20 15:36:32 +0400

    fixed navbar bug

--- src/components/shared/navbar/MobileNav.tsx
+++ src/components/shared/navbar/MobileNav.tsx
@@ -1,10 +1,9 @@
 "use client";
 
-import React from "react";
 import {
   Sheet,
-  SheetClose,
   SheetContent,
+  SheetClose,
   SheetTrigger,
 } from "@/components/ui/sheet";
 import Image from "next/image";
@@ -16,17 +15,25 @@ import { usePathname } from "next/navigation";
 
 const NavContent = () => {
   const pathname = usePathname();
+
   return (
     <section className="flex h-full flex-col gap-6 pt-16">
       {sidebarLinks.map((item) => {
         const isActive =
           (pathname.includes(item.route) && item.route.length > 1) ||
           pathname === item.route;
+
+        // TODO
+
         return (
-          <SheetClose key={item.route}>
+          <SheetClose asChild key={item.route}>
             <Link
               href={item.route}
-              className={`${isActive ? "primary-gradient rounded-lg text-light-900" : "text-dark300_light900"} flex items-center justify-start gap-4 bg-transparent py-4`}
+              className={`${
+                isActive
+                  ? "primary-gradient rounded-lg text-light-900"
+                  : "text-dark300_light900"
+              } flex items-center justify-start gap-4 bg-transparent p-4`}
             >
               <Image
                 src={item.imgURL}
@@ -52,25 +59,26 @@ const MobileNav = () => {
       <SheetTrigger asChild>
         <Image
           src="/assets/icons/hamburger.svg"
-          className="invert-colors sm:hidden"
           width={36}
           height={36}
           alt="Menu"
+          className="invert-colors sm:hidden"
         />
       </SheetTrigger>
       <SheetContent
         side="left"
         className="background-light900_dark200 border-none"
       >
-        <Link href={"/"} className="flex items-center gap-2">
+        <Link href="/" className="flex items-center gap-1">
           <Image
             src="/assets/images/site-logo.svg"
             width={23}
             height={23}
-            alt="stack overflow v2"
+            alt="DevFlow"
           />
-          <p className="h2-bold  text-dark100_light900 dark:text-light-900 max-sm:hidden">
-            Stack Overflow <span className="text-primary-500">V2</span>
+
+          <p className="h2-bold text-dark100_light900 font-spaceGrotesk">
+            Dev <span className="text-primary-500">Overflow</span>
           </p>
         </Link>
         <div>
@@ -87,9 +95,10 @@ const MobileNav = () => {
                   </Button>
                 </Link>
               </SheetClose>
+
               <SheetClose asChild>
                 <Link href="/sign-up">
-                  <Button className="small-medium text-dark400_light900 light-border-2 btn-tertiary min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none">
+                  <Button className="small-medium light-border-2 btn-tertiary text-dark400_light900 min-h-[41px] w-full rounded-lg border px-4 py-3 shadow-none">
                     Sign Up
                   </Button>
                 </Link>
--- src/components/shared/navbar/Theme.tsx
+++ src/components/shared/navbar/Theme.tsx
@@ -1,5 +1,9 @@
 "use client";
 
+import React from "react";
+
+import { useThemeContext } from "@/context/ThemeProvider";
+
 import {
   Menubar,
   MenubarContent,
@@ -7,59 +11,60 @@ import {
   MenubarMenu,
   MenubarTrigger,
 } from "@/components/ui/menubar";
-import { themes } from "@/constants";
-import { useThemeContext } from "@/context/ThemeProvider";
 import Image from "next/image";
+import { themes } from "@/constants";
 
 const Theme = () => {
   const { mode, setMode } = useThemeContext();
-  const isDark = mode === "dark";
-
-  const imageConfig = {
-    height: 20,
-    width: 20,
-    src: `/assets/icons/${isDark ? "moon.svg" : "sun.svg"}`,
-    alt: isDark ? "moon" : "sun",
-  };
 
   return (
     <Menubar className="relative border-none bg-transparent shadow-none">
       <MenubarMenu>
-        <MenubarTrigger className="data[state=open]:bg-light-900 dark:data[state=open]:bg-dark-200 focus:bg-light-900 dark:focus:bg-dark-200">
-          <Image
-            src={imageConfig.src}
-            height={imageConfig.height}
-            width={imageConfig.width}
-            alt={imageConfig.alt}
-            className="active-theme"
-          />
+        <MenubarTrigger className="focus:bg-light-900 data-[state=open]:bg-light-900 dark:focus:bg-dark-200 dark:data-[state=open]:bg-dark-200">
+          {mode === "light" ? (
+            <Image
+              src="/assets/icons/sun.svg"
+              alt="sun"
+              width={20}
+              height={20}
+              className="active-theme"
+            />
+          ) : (
+            <Image
+              src="/assets/icons/moon.svg"
+              alt="moon"
+              width={20}
+              height={20}
+              className="active-theme"
+            />
+          )}
         </MenubarTrigger>
-        <MenubarContent className="absolute -right-12 mt-3 min-w-[120px] rounded border py-2 dark:border-dark-400 dark:bg-dark-300">
-          {themes.map(({ value, icon, label }) => (
+        <MenubarContent className="absolute -right-12 mt-3 min-w-[120px] rounded border bg-white py-2 dark:border-dark-400 dark:bg-dark-300">
+          {themes.map((item) => (
             <MenubarItem
-              key={icon}
-              className="flex items-center gap-4 px-2.5 py-2 dark:focus:bg-dark-400"
+              key={item.value}
+              className="flex items-center gap-4 px-2.5 py-2 hover:bg-gray-200 dark:focus:bg-dark-400"
               onClick={() => {
-                setMode(value);
+                setMode(item.value);
 
-                if (value !== "system") {
-                  localStorage.theme = value;
+                if (item.value !== "system") {
+                  localStorage.theme = item.value;
                 } else {
                   localStorage.removeItem("theme");
                 }
               }}
             >
               <Image
-                src={icon}
-                alt={value}
+                src={item.icon}
+                alt={item.value}
                 width={16}
                 height={16}
-                className={`${mode === value && "active-theme"}`}
+                className={`${mode === item.value && "active-theme"}`}
               />
               <p
-                className={`body-semibold text-light-500 ${mode === value ? "text-primary-500" : "text-dark100_light900"}`}
+                className={`body-semibold text-light-500 ${mode === item.value ? "text-primary-500" : "text-dark100_light900"}`}
               >
-                {label}
+                {item.label}
               </p>
             </MenubarItem>
           ))}
